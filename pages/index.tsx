1a7d954d5e763440781b4c55779e952daac4f7ae
Satoshi Watanabe <sassy.watanabe@gmail.com>
2020-07-01 01:12:48 +0900

    first commit

---
+++ pages/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Head from 'next/head'
+
+export default function Home() {
+    return (
+        <div>
+            <Head>
+                <title>hello</title>
+            </Head>
+            <main>
+                TestPage
+            </main>
+        </div>
+
+    )
+}
